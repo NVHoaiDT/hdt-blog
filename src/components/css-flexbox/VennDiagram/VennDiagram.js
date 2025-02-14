5e83e17a829e59e7e38273f5452b7472a71ec4f0
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-02-14 20:30:29 +0700

    add framer-motion for venn digram

--- src/components/css-flexbox/VennDiagram/VennDiagram.js
+++ src/components/css-flexbox/VennDiagram/VennDiagram.js
@@ -1,11 +1,29 @@
+"use client";
+
 import React from "react";
+
+import { motion } from "framer-motion";
 
 function VennDiagram() {
   return (
     <>
-      <svg height="300" width="300" xmlns="http://www.w3.org/2000/svg">
-        <circle r="100" cx="100" cy="100" fill="red" fillOpacity="0.35" />
-        <circle r="100" cx="200" cy="100" fill="blue" fillOpacity="0.35" />
+      <svg height="800" width="1000" xmlns="http://www.w3.org/2000/svg">
+        <motion.circle
+          r="180"
+          cx="300"
+          cy="300"
+          fill="red"
+          fillOpacity="0.35"
+          whileHover={{ scale: 1.1, x: -100 }}
+        />
+        <motion.circle
+          r="180"
+          cx="500"
+          cy="300"
+          fill="blue"
+          fillOpacity="0.35"
+          whileHover={{ scale: 1.1, x: 100 }}
+        />
       </svg>
     </>
   );
